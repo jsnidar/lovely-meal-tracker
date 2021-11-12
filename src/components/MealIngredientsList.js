56e7ea302997e0554f2be333ae78f5bb939a9a62
Jeremiah <jsnidar@gmail.com>
2021-11-12 15:05:26 -0600

    Mealcard pluralizes units when unit is each, converts decimal values to fractions and mixed numbers,  and pluralizes cups when unit is cup

--- src/components/MealIngredientsList.js
+++ src/components/MealIngredientsList.js
@@ -5,8 +5,20 @@ const MealIngredientsList = ({ mealIngredients }) => {
 
   
   let ingredientsList = mealIngredients.map ((ing) => {
+  
+  const Fraction = require('fractional').Fraction
+  const decimalValue = +(ing.ingredient.portion_quantity * ing.quantity).toPrecision(2)
+  const quantity = (new Fraction(decimalValue).toString())
+  let pluralize = require('pluralize')
+  
+  console.log(decimalValue)
+  let unit = ing.ingredient.portion_unit
+  if (unit === 'cup' && decimalValue > 1) {
+    unit = pluralize(unit, decimalValue)
+  }
+
   return <ListGroup.Item key={ing.id}>
-    {ing.ingredient.portion_quantity * ing.quantity} {ing.ingredient.portion_unit} {ing.ingredient.name}
+    {quantity} {unit === 'each' ? "" : unit} {unit === 'each' ? pluralize(ing.ingredient.name, decimalValue): ing.ingredient.name}
   </ListGroup.Item>
   })
   return (
